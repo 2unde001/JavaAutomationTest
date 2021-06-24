81ea7f6aa5714365143f3d1ba385e090002b9da6
Tunde <tunde0011@hotmail.com>
2021-06-24 07:00:21 +0100

    Getting started with selenium automation test

--- beakonhub/src/test/java/com/beakonhub/app/AppTest.java
+++ beakonhub/src/test/java/com/beakonhub/app/AppTest.java
@@ -1,19 +1,67 @@
 package com.beakonhub.app;
 
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.pagefactory.ElementLocator;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest
 {
+    WebDriver driver;
+
+    /**
+     * Set up WebDriverManager launch and manager driver in this instance chromedriver.
+     * Navigate to github to verify WebDriver set up
+     */
     @Test
-    public void firstTest()
-    {
-        int result = 2 + 2;
-        Assert.assertEquals(4, result);
+    public void driverManagerSetUp() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.navigate().to("https://github.com/");
+
+        // Find element by className
+        driver.findElement(By.className("mr-4"));
+        assertTrue("Github Logo display", true);
+        Thread.sleep(5);
+
+
+        // Find element by xpath
+        WebElement clickMarketPlace = driver.findElement(By.xpath("//a[@href='/marketplace']"));
+        clickMarketPlace.click();
+        Thread.sleep(3);
+
+        // Find element by cssSelector
+        WebElement freeApps = driver.findElement(By.cssSelector("a[class=\"btn-mktg btn-outline-mktg\"]"));
+        freeApps.click();
+        Thread.sleep(3);
+
+        // Verify free app page is open
+        WebElement appPage = driver.findElement(By.cssSelector("h1[class='h2']"));
+        System.out.print(appPage.getText());
+
+
+
+        driver.close();
+
     }
+
+
+
+
 
 }
---
+++ beakonhub/src/test/java/com/beakonhub/app/elementsLocator.md
@@ -0,0 +1,17 @@
+#### Element locator stratergy.
+Driver need to interact with the page element in order to perform or automate a website.
+
+This element(s) are HTML (Hyper Text Markup Language) used by developer to develop a webite .
+
+
+We can locate page element using several locators such as:
+- Locate element by id
+- Locate element by classname
+- Locate element by cssSelector
+- Locate element by name
+- Locate element by xpath
+- Locate element tby linkText
+
+Note: 
+I'm using **Thread.sleep()** method to let the Dom(document object model) to settle down - lett the page pull all the elelments before procedding to the next action.
+I will use more implicit and explicit wait laterin the section.
